e67b803cc3bdefbd0a01ecb882a8c0385bc2e0f8
JL <lambertjefferson66@gmail.com>
2025-02-06 17:30:33 -0600

    numbers getting therer

--- lib/screens/whiteboard_screen.dart
+++ lib/screens/whiteboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
+import 'dart:math' as math;
 
 class WhiteboardScreen extends StatefulWidget {
   const WhiteboardScreen({super.key});
@@ -28,7 +29,13 @@ class _WhiteboardScreenState extends State<WhiteboardScreen> with SingleTickerPr
 
   void _initializePaths() {
     // Create paths for each character/symbol in the equation
-    final characters = ['1', 'x', ' ', '+', ' ', 'y', ' ', '=', ' ', '1234', ' ', '-', ' ', '10', ' ', '/', ' ', '22', '^', '3'];
+    final characters = [
+      '1', ' ', '2', ' ', '3', ' ', '4', ' ', '5', ' ', '6', ' ', '7', ' ', '8', ' ', '9',
+      '\n',
+      '123456789',
+      '\n',
+      '+', ' ', '-', ' ', '×', ' ', '÷', ' ', '=', ' ', '^', ' ', '(', ' ', ')', ' ', '√'
+    ];
     double x = 50; // Starting x position
     double y = 200; // Vertical position
     
@@ -36,9 +43,106 @@ class _WhiteboardScreenState extends State<WhiteboardScreen> with SingleTickerPr
       final path = Path();
       switch (char) {
         case '1':
-          path.moveTo(x + 5, y - 20);
-          path.lineTo(x + 5, y + 20);
-          x += 20;
+          path.moveTo(x + 2, y - 13);   // Start from top-left, slightly lower
+          path.lineTo(x + 10, y - 15);  // Angle up to top of stem
+          path.lineTo(x + 10, y + 15);  // Vertical line straight down
+          path.lineTo(x, y + 15);       // Base left
+          path.lineTo(x + 20, y + 15);  // Base right
+          x += 25;
+          break;
+        case '2':
+          path.moveTo(x + 3, y - 15);     // Start more left
+          path.quadraticBezierTo(x + 20, y - 15, x + 15, y);  // Top curve (peaks left)
+          path.quadraticBezierTo(x + 12, y + 10, x, y + 15);  // Bottom curve (more open in middle)
+          path.lineTo(x + 17, y + 15);    // Bottom line (aligned with "1")
+          x += 25;
+          break;
+        case '3':
+          path.moveTo(x + 2, y - 15);     // Start top left
+          path.quadraticBezierTo(x + 18, y - 15, x + 16, y - 8);  // Top bowl curves right
+          path.quadraticBezierTo(x + 14, y - 2, x + 8, y);      // Connect to middle point, much more left
+          path.quadraticBezierTo(x + 14, y + 2, x + 16, y + 8);  // Start of bottom bowl
+          path.quadraticBezierTo(x + 18, y + 15, x + 2, y + 15); // Bottom bowl curves back left
+          x += 25;
+          break;
+        case '4':
+          path.moveTo(x + 15, y + 15);  // Bottom of vertical
+          path.lineTo(x + 15, y - 15);  // Up vertical
+          path.lineTo(x + 3, y + 5);    // Diagonal to left (moved in from x)
+          path.lineTo(x + 20, y + 5);   // Horizontal line
+          x += 25;
+          break;
+        case '5':
+          path.moveTo(x + 20, y - 15);  // Top right
+          path.lineTo(x, y - 15);       // Top left
+          path.lineTo(x, y);            // Down vertical
+          path.quadraticBezierTo(x + 15, y, x + 15, y + 5);  // Bottom curve (smaller)
+          path.quadraticBezierTo(x + 15, y + 16, x, y + 16);  // Bottom curve (slightly lower)
+          path.lineTo(x + 20, y + 16);  // Bottom line
+          x += 25;
+          break;
+        case '6':
+          // Calculate relative dimensions for the 6
+          final width = 20.0;  // Base width for the number
+          final height = 30.0; // Base height for the number
+          
+          // Top curve of 6
+          path.moveTo(x + width / 1.4, y - height/2);
+          path.cubicTo(
+            x + width / 2 + 10, y - height/2 - 5,  // First control point
+            x + width / 4, y - height/4,           // Second control point
+            x + width / 2.9, y + height/2.8        // End point
+          );
+          
+          // Bottom circle of 6
+          path.addArc(
+            Rect.fromCenter(
+              center: Offset(x + width/2 + 2, y + height/6),
+              width: width/1.2,
+              height: width/1.2
+            ),
+            math.pi,
+            2 * math.pi
+          );
+          
+          x += width + 5;
+          break;
+        case '7':
+          path.moveTo(x, y - 15);       // Top left
+          path.lineTo(x + 20, y - 15);  // Top right
+          path.lineTo(x, y + 15);       // Diagonal down
+          x += 25;
+          break;
+        case '8':
+          path.addOval(Rect.fromCenter(
+            center: Offset(x + 10, y - 6),
+            width: 16,
+            height: 16,
+          ));
+          path.addOval(Rect.fromCenter(
+            center: Offset(x + 10, y + 6),
+            width: 16,
+            height: 16,
+          ));
+          x += 25;
+          break;
+        case '9':
+          path.addOval(Rect.fromCenter(
+            center: Offset(x + 10, y + 5),
+            width: 20,
+            height: 30,
+          ));
+          path.moveTo(x + 10, y - 15);
+          path.lineTo(x + 10, y + 15);
+          x += 25;
+          break;
+        case '0':
+          path.addOval(Rect.fromCenter(
+            center: Offset(x + 10, y),
+            width: 20,
+            height: 30,
+          ));
+          x += 25;
           break;
         case 'x':
           path.moveTo(x, y - 15);
@@ -54,36 +158,88 @@ class _WhiteboardScreenState extends State<WhiteboardScreen> with SingleTickerPr
           path.lineTo(x + 20, y);
           x += 30;
           break;
-        case 'y':
-          path.moveTo(x, y - 15);
-          path.lineTo(x + 7, y);
-          path.lineTo(x + 14, y - 15);
-          path.moveTo(x + 7, y);
-          path.lineTo(x + 7, y + 15);
-          x += 24;
-          break;
-        case '=':
-          path.moveTo(x, y - 5);
-          path.lineTo(x + 20, y - 5);
-          path.moveTo(x, y + 5);
-          path.lineTo(x + 20, y + 5);
-          x += 30;
-          break;
         case '-':
           path.moveTo(x, y);
           path.lineTo(x + 20, y);
           x += 30;
           break;
-        case '/':
-          path.moveTo(x, y - 15);
-          path.lineTo(x + 15, y + 15);
-          x += 25;
+        case '×':
+          path.moveTo(x + 10, y - 10);
+          path.lineTo(x + 10, y + 10);
+          path.moveTo(x, y);
+          path.lineTo(x + 20, y);
+          x += 30;
+          break;
+        case '÷':
+          // Top dot
+          path.addOval(Rect.fromCenter(
+            center: Offset(x + 10, y - 12),
+            width: 4,
+            height: 4,
+          ));
+          // Division line
+          path.moveTo(x, y);
+          path.lineTo(x + 20, y);
+          // Bottom dot
+          path.addOval(Rect.fromCenter(
+            center: Offset(x + 10, y + 12),
+            width: 4,
+            height: 4,
+          ));
+          x += 30;
+          break;
+        case '=':
+          // Two parallel lines with more spacing
+          path.moveTo(x, y - 8);
+          path.lineTo(x + 20, y - 8);
+          path.moveTo(x, y + 8);
+          path.lineTo(x + 20, y + 8);
+          x += 30;
           break;
         case '^':
           path.moveTo(x, y);
           path.lineTo(x + 5, y - 10);
           path.lineTo(x + 10, y);
           x += 15;
+          break;
+        case '(':
+          path.moveTo(x + 10, y - 15);
+          path.lineTo(x + 10, y + 15);
+          path.addArc(
+            Rect.fromCenter(
+              center: Offset(x + 10, y),
+              width: 20,
+              height: 30,
+            ),
+            0,
+            math.pi
+          );
+          x += 25;
+          break;
+        case ')':
+          path.moveTo(x + 10, y - 15);
+          path.lineTo(x + 10, y + 15);
+          path.addArc(
+            Rect.fromCenter(
+              center: Offset(x + 10, y),
+              width: 20,
+              height: 30,
+            ),
+            math.pi,
+            math.pi
+          );
+          x += 25;
+          break;
+        case '√':
+          path.moveTo(x, y - 15);
+          path.lineTo(x + 20, y - 15);
+          path.moveTo(x + 20, y - 15);
+          path.lineTo(x + 10, y + 15);
+          x += 25;
+          break;
+        case '\n':
+          y += 50;
+          x = 50;
           break;
         case ' ':
           x += 10;
@@ -93,30 +249,116 @@ class _WhiteboardScreenState extends State<WhiteboardScreen> with SingleTickerPr
           if (char.length > 1) {
             for (var digit in char.split('')) {
               final digitPath = Path();
-              digitPath.moveTo(x, y - 20);
-              digitPath.lineTo(x, y + 20);
+              switch (digit) {
+                case '1':
+                  digitPath.moveTo(x + 2, y - 13);   // Start from top-left, slightly lower
+                  digitPath.lineTo(x + 10, y - 15);  // Angle up to top of stem
+                  digitPath.lineTo(x + 10, y + 15);  // Vertical line straight down
+                  digitPath.lineTo(x, y + 15);       // Base left
+                  digitPath.lineTo(x + 20, y + 15);  // Base right
+                  break;
+                case '2':
+                  digitPath.moveTo(x + 3, y - 15);     // Start more left
+                  digitPath.quadraticBezierTo(x + 20, y - 15, x + 15, y);  // Top curve (peaks left)
+                  digitPath.quadraticBezierTo(x + 12, y + 10, x, y + 15);  // Bottom curve (more open in middle)
+                  digitPath.lineTo(x + 17, y + 15);    // Bottom line (aligned with "1")
+                  break;
+                case '3':
+                  digitPath.moveTo(x + 2, y - 15);     // Start top left
+                  digitPath.quadraticBezierTo(x + 18, y - 15, x + 16, y - 8);  // Top bowl curves right
+                  digitPath.quadraticBezierTo(x + 14, y - 2, x + 8, y);      // Connect to middle point, much more left
+                  digitPath.quadraticBezierTo(x + 14, y + 2, x + 16, y + 8);  // Start of bottom bowl
+                  digitPath.quadraticBezierTo(x + 18, y + 15, x + 2, y + 15); // Bottom bowl curves back left
+                  break;
+                case '4':
+                  digitPath.moveTo(x + 15, y + 15);  // Bottom of vertical
+                  digitPath.lineTo(x + 15, y - 15);  // Up vertical
+                  digitPath.lineTo(x + 3, y + 5);    // Diagonal to left (moved in from x)
+                  digitPath.lineTo(x + 20, y + 5);   // Horizontal line
+                  break;
+                case '5':
+                  digitPath.moveTo(x + 20, y - 15);  // Top right
+                  digitPath.lineTo(x, y - 15);       // Top left
+                  digitPath.lineTo(x, y);            // Down vertical
+                  digitPath.quadraticBezierTo(x + 15, y, x + 15, y + 5);  // Bottom curve (smaller)
+                  digitPath.quadraticBezierTo(x + 15, y + 16, x, y + 16);  // Bottom curve (slightly lower)
+                  digitPath.lineTo(x + 20, y + 16);  // Bottom line
+                  break;
+                case '6':
+                  // Calculate relative dimensions for the 6
+                  final width = 20.0;
+                  final height = 30.0;
+                  
+                  digitPath.moveTo(x + width / 1.4, y - height/2);
+                  digitPath.cubicTo(
+                    x + width / 2 + 10, y - height/2 - 5,
+                    x + width / 4, y - height/4,
+                    x + width / 2.9, y + height/2.8
+                  );
+                  
+                  digitPath.addArc(
+                    Rect.fromCenter(
+                      center: Offset(x + width/2 + 2, y + height/6),
+                      width: width/1.2,
+                      height: width/1.2
+                    ),
+                    math.pi,
+                    2 * math.pi
+                  );
+                  break;
+                case '7':
+                  digitPath.moveTo(x, y - 15);       // Top left
+                  digitPath.lineTo(x + 20, y - 15);  // Top right
+                  digitPath.lineTo(x, y + 15);       // Diagonal down
+                  break;
+                case '8':
+                  digitPath.addOval(Rect.fromCenter(
+                    center: Offset(x + 10, y - 6),
+                    width: 16,
+                    height: 16,
+                  ));
+                  digitPath.addOval(Rect.fromCenter(
+                    center: Offset(x + 10, y + 6),
+                    width: 16,
+                    height: 16,
+                  ));
+                  break;
+                case '9':
+                  digitPath.addOval(Rect.fromCenter(
+                    center: Offset(x + 10, y + 5),
+                    width: 20,
+                    height: 30,
+                  ));
+                  digitPath.moveTo(x + 10, y - 15);
+                  digitPath.lineTo(x + 10, y + 15);
+                  break;
+                case '0':
+                  digitPath.addOval(Rect.fromCenter(
+                    center: Offset(x + 10, y),
+                    width: 20,
+                    height: 30,
+                  ));
+                  break;
+              }
               _paths.add(digitPath);
-              _pathLengths.add(40);
-              _totalLength += 40;
-              x += 20;
+              _pathLengths.add(60); // Increased path length for more complex numbers
+              _totalLength += 60;
+              x += 25;
             }
-          } else {
-            // Single digit
-            path.moveTo(x, y - 20);
-            path.lineTo(x, y + 20);
-            x += 20;
           }
       }
       
-      if (char != ' ') {
+      if (char != ' ' && char != '\n') {
         final pathMetrics = path.computeMetrics();
         double pathLength = 0;
         for (final metric in pathMetrics) {
           pathLength += metric.length;
         }
-        _paths.add(path);
-        _pathLengths.add(pathLength);
-        _totalLength += pathLength;
+        if (pathLength > 0) {
+          _paths.add(path);
+          _pathLengths.add(pathLength);
+          _totalLength += pathLength;
+        }
       }
     }
   }
